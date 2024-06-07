81a516944c812ec6b4333a2e4c42af7e3f37b8e0
Jens Bengtsson <jb225je@student.lnu.se>
2024-06-07 14:54:40 +0200

    crud

--- routes/index.mjs
+++ routes/index.mjs
@@ -9,35 +9,33 @@ const users = rawData.users;
 const posts = rawData.posts;
 
 router.get("/", (request, response) => {
-  response.render("index", { users, posts });
-  
+    response.render("index", { users, posts });
+
 });
 
 router.post("/", (request, response) => {
-  const { body } = request;
-  let requestPostId = { id: posts[posts.length - 1].id + 1, ...body };
-  console.log(requestPostId, 'requestPostId')
-  posts.push(requestPostId );
-  console.log(posts, 'posts')
-  return response.status(201);
+    const { body } = request;
+    let requestPostId = { id: posts[posts.length - 1].id + 1, ...body };
+    posts.push(requestPostId);
+    return response.status(201);
 });
 
 router.put("/post/:id", getPostId, (request, response) => {
-  const { body } = request;
-  posts[request.postId ] = { id: posts[request.postId ].id, ...body };
-  return response.sendStatus(200);
+    const { body } = request;
+    posts[request.postId] = { id: posts[request.postId].id, ...body };
+    return response.sendStatus(200);
 });
 
 
 router.patch("/post/:id", getPostId, (request, response) => {
-  const { body } = request;
-  posts[request.postId ] = { ...posts[request.postId], ...body };
-  return response.sendStatus(200);
+    const { body } = request;
+    posts[request.postId] = { ...posts[request.postId], ...body };
+    return response.sendStatus(200);
 });
 
 router.delete("/post/:id", getPostId, (request, response) => {
-  posts.splice(request.postId, 1);
-  return response.status(204).send({msg: `You deleted post with header: ${posts[request.postId]}`});
+    posts.splice(request.postId, 1);
+    return response.status(204).send({ msg: `You deleted post with header: ${posts[request.postId]}` });
 });
 
 export { router as postRouter };
