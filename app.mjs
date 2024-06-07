619768e67cb8d07abfee9357f848f3411265b0e4
Oscar Donaldson <oscar@donaldson.se>
2024-06-07 10:27:33 +0200

    bladsa

--- app.mjs
+++ app.mjs
@@ -1,8 +1,9 @@
 import express from "express"
 import path from 'path'
+import { readFile } from "fs";
 import { fileURLToPath } from 'url';
 import { detailRouter } from "./routes/detail.mjs"
-import { postRouter } from "./routes/post.mjs"
+import { postRouter } from "./routes/index.mjs"
 
 const app = express();
 const PORT = 3000;
@@ -11,24 +12,30 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Ställ in EJS som vy-motor
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Statiska filer (om du har några)
 app.use(express.static('views'));
 app.use(express.static(__dirname + '/public'));
 
-// Ställ in EJS som vy-motor
-app.set('view engine', 'ejs');
-app.set('views', './views');
 
-app.get('/', (request, response) => {
-    response.render("index")
+
+app.get('/', async (request, response) => {
+    try {
+        const data = await readFile(new URL('./mockdata/users.json', import.meta.url), 'utf-8')
+        const jsonData = JSON.parse(data)
+        response.render('index', {data: jsonData})
+    } catch (err) {
+        console.error('Error reading JSON file:', err)
+        response.status(500).send('Internal Server Error')
+    }
+    
 })
 
 app.get('/detail', (request, response) => {
     response.render('detail')
-})
-
-app.get('/posts', (request, response) => {
-    response.render('post')
 })
 
 app.listen(PORT, () => {
