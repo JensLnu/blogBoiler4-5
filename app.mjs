0f4e7ad5dc80ac9515990a0053df4c8041852abe
David Heidari <davidheidari2@gmail.com>
2024-06-07 10:23:47 +0200

    imported json-file to app.mjs

--- app.mjs
+++ app.mjs
@@ -1,11 +1,20 @@
 import express from "express"
 import path from 'path'
+import fs from "fs";
 import { fileURLToPath } from 'url';
 import { detailRouter } from "./routes/detail.mjs"
 import { postRouter } from "./routes/post.mjs"
 
+app.use(express.json());
 const app = express();
+app.use(express.static('public'));
 const PORT = 3000;
+
+const rawData =JSON.parse(fs.readFileSync('./mockdata/users.json', 'utf-8'));
+const users = rawData.users
+const posts = rawData.posts
+
+
 
 // Hämta den aktuella filens sökväg och katalog
 const __filename = fileURLToPath(import.meta.url);
@@ -19,11 +28,14 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/', (req, res) => {
+app.get('/', (_request, _response) => {
+    // läs in 'users' och 'posts' till er EJS fil Oscar/Nyat
     res.render("index")
 })
 
 
 app.listen(PORT, () => {
     console.log(`App is running on: localhost:${PORT}`);
+    // console.log(users, 'users')
+    // console.log(posts, 'posts')
 });
