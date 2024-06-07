bb0905a14f9a3b776778617b2cc0bbf7514650bc
Oscar Donaldson <oscar@donaldson.se>
2024-06-07 09:32:24 +0200

    Structure app.mjs

--- app.mjs
+++ app.mjs
@@ -1,11 +1,21 @@
 import express from "express"
-
+import path from 'path'
 import { postRouter } from "./routes/post.mjs"
 
 const app = express()
 const PORT = 3000
 
+// Hämta den aktuella filens sökväg och katalog
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Statiska filer (om du har några)
+app.use(express.static('views'));
+app.use(express.static(__dirname + '/public'));
+
+// Ställ in EJS som vy-motor
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(PORT, () => {
     console.log(`App is running on: localhost:${PORT}`)
