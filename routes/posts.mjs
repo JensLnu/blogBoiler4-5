5c6db46c62537371c2292164da92980f75b4edae
Emil <emil.winberg1@gmail.com>
2024-06-10 12:30:50 +0200

    Add expressurlencoded to user router and form handling

--- routes/posts.mjs
+++ routes/posts.mjs
@@ -10,35 +10,53 @@ import {createPostValidationSchema } from "../validationSchemas/createPostValida
 import { getTodaysDate } from "../middelware/getTodaysDate.mjs";
 
 const router = express.Router();
+router.use(express.urlencoded({ extended: true}))
 
 const updateDb = () => {
     fs.writeFileSync(pathToPosts, JSON.stringify(posts, null, 2), 'utf8');
 }
-
 // Validation för denna, kolla att bodyn är korrekt formaterad.
 router.post("/", checkSchema(createPostValidationSchema), getTodaysDate, (request, response) => {
     // validation
+    
     const resultErrors = validationResult(request)
     if (!resultErrors.isEmpty()) {
         return response.status(400).send({ errors: resultErrors.array() })
-    } 
-
-    // här behöver också 
-    /*
-    "postedBy": "David",
-    "postedByImage": "../images/profilePics/david.png",
-    "postDate": request.todaysDate // lägg till på den nya posten
-    "postImg": "../images/Dr-manhatan.png",
-    "commentsOnPost": [
-      {
+        } 
+        
+        // här behöver också 
+        /*
+        "header": "Batman",
+        "content": "Super power: Very rich",
+        "tag": "cool",
         "postedBy": "David",
-        "comment": "Testing...",
-        "likes": 10
-      }
-    ],
-    "likes": 50,
-    "shared": 20
-    */
+        "postedByImage": "../images/profilePics/david.png",
+        "postDate": request.todaysDate // lägg till på den nya posten
+        "postImg": "../images/Dr-manhatan.png",
+        "commentsOnPost": [
+            {
+                "postedBy": "David",
+                "comment": "Testing...",
+                "likes": 10
+                }
+                ],
+                "likes": 50,
+                "shared": 20
+                */
+
+    // HÄMTA URL FRÅN USERS BASERAT PÅ ANVÄNDARNAMN SOM SKICKAS I BODYN
+    // LÄGG TILL DAGENS DATUM
+    // LÄGG TILL MÖJLIGHETEN ATT LÄGGA TILL BILD I FORMEN
+    // SÄTT HEADER TILL TITLE FRÅN BODY
+    // SÄTT CONTENT TILL BODY.content
+    // INPUT FÖR TAGS? ELLER TAGS FRÅN CONTENT?
+    // SÄTT COMMENTSONPOST TILL TOM ARRAY
+    // SÄTT LIKES OCH SHARES TILL 0
+    // VISA ERROR MESSAGE I BROWSER ISTÄLLET FÖR AT SKRIVA UT ERROR-LOGGEN
+
+    // FIXA MODAL ELLER IN-LINE FÖR FORMEN
+    // FIXA DISPLAY FÖR POSTS (EN I TAGET, INTE FLERA PÅ SAMMA RAD)
+
     const { body } = request;
     let requestPostId = { id: nanoid(), ...body };
     posts.push(requestPostId);
