bb6482cb60de3ac4f8375e1a0120b55f4e1f0f1b
Jens Bengtsson <jb225je@student.lnu.se>
2024-06-14 16:31:25 +0200

    test atlas

--- database/connectToMongoDb.mjs
+++ database/connectToMongoDb.mjs
@@ -2,7 +2,9 @@ import mongoose from "mongoose"
 
 // const uri = "mongodb+srv://JensaFixar:wgBSOn9ivoiqvf6d@gbgBlogBoiler.f1lv3mv.mongodb.net/?retryWrites=true&w=majority&appName=JensFixar"
 // const uri = "mongodb+srv://davidheidari2:9GD3YTPcU8s2i3LE@gbgBlog.bydwz4d.mongodb.net/"
-const uri = "mongodb+srv://JensaFixar:wgBSOn9ivoiqvf6d@jensfixar.f1lv3mv.mongodb.net/"
+// const uri = "mongodb+srv://JensaFixar:wgBSOn9ivoiqvf6d@jensfixar.f1lv3mv.mongodb.net/"
+
+const uri = "mongodb+srv://JensaFixar:wgBSOn9ivoiqvf6d@jensfixar.f1lv3mv.mongodb.net/gbgBlogBoiler?retryWrites=true&w=majority"
 
 
 // fungerar lokalt 
