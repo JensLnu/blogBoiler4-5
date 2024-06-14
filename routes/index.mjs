bd12ed5d401d3b6f85ace8b4816e839b69418dcd
Jens Bengtsson <jb225je@student.lnu.se>
2024-06-14 11:24:07 +0200

    json agian

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,17 +1,17 @@
 import express from "express";
-import { userModel } from "../models/userModel.mjs";
-import { postModel } from "../models/postModel.mjs";
+// import { userModel } from "../models/userModel.mjs";
+// import { postModel } from "../models/postModel.mjs";
 
 // OLD NEWS tidigare från JSON filen nu från mongoDb
-// import { users } from "../app.mjs";
-// import { posts } from "../app.mjs";
+import { users } from "../app.mjs";
+import { posts } from "../app.mjs";
 
 const router = express.Router();
 
 router.get("/", async (request, response) => {
     try {
-        let posts = await postModel.find()
-        let users = await userModel.find()
+        // let posts = await postModel.find()
+        // let users = await userModel.find()
         response.render("index", { users, posts });
     }
     catch (error) {
